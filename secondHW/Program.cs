277a945acd3d8a0d690f8c06fede68d1470357bf
NazaninRzQ <nazanin.rezaee.ghotabadi@gmail.com>
2024-04-13 15:11:32 +0330

    Added homeworkNum2 to the repo

---
+++ secondHW/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Management;
+using System.Diagnostics;
+
+class FlashDriveDetector
+{
+    static void Main(string[] args)
+    {
+        ManagementEventWatcher watcher = new ManagementEventWatcher();
+        WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2");
+
+        watcher.Query = query;
+        watcher.EventArrived += (s, e) =>
+        {
+            Console.WriteLine("Flash Drive Inserted!");
+            Process.Start("mspaint.exe");
+        };
+
+        watcher.Start();
+
+        Console.WriteLine("Waiting for flash drive insertion. Press any key to exit...");
+        Console.ReadKey();
+
+        watcher.Stop();
+    }
+}
