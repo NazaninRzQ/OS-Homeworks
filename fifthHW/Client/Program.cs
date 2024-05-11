c4f8e2670bad7cdda4cc1a19da27dab8e0d70a16
NazaninRzQ <nazanin.rezaee.ghotabadi@gmail.com>
2024-05-11 13:36:00 +0330

    Homework5 is done.

---
+++ fifthHW/Client/Program.cs
@@ -0,0 +1,58 @@
+﻿﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class TcpClientSample
+{
+    private const int BufferSize = 1024;
+
+    public static void Main()
+    {
+        int port;
+        string filePath;
+
+        Console.WriteLine("Please enter the port number of the server:");
+        port = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Please enter the path of the file to send:");
+        filePath = Console.ReadLine();
+
+        try
+        {
+            using (TcpClient client = new TcpClient("127.0.0.1", port))
+            {
+                Console.WriteLine("Connected to the server...");
+
+                using (NetworkStream networkStream = client.GetStream())
+                using (FileStream fileStream = File.OpenRead(filePath))
+                {
+                    byte[] buffer = new byte[BufferSize];
+                    int bytesRead;
+
+                    while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        networkStream.Write(buffer, 0, bytesRead);
+                    }
+
+                    Console.WriteLine("File sent successfully.");
+                }
+            }
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Unable to connect to the server.");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File not found.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
---
+++ fifthHW/Server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class Program
+{
+    private const int BufferSize = 1024;
+
+    public static void Main()
+    {
+        int port;
+
+        Console.WriteLine("Please enter the port number to listen on:");
+        port = Int32.Parse(Console.ReadLine());
+
+        try
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            TcpListener tcpServer = new TcpListener(localAddr, port);
+            tcpServer.Start();
+
+            Console.WriteLine("Server started. Listening for incoming connections...");
+
+            while (true)
+            {
+                TcpClient client = tcpServer.AcceptTcpClient();
+                Console.WriteLine("Client connected.");
+
+                using (NetworkStream networkStream = client.GetStream())
+                using (FileStream fileStream = File.Create("received_file.dat"))
+                {
+                    byte[] buffer = new byte[BufferSize];
+                    int bytesRead;
+
+                    while ((bytesRead = networkStream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        fileStream.Write(buffer, 0, bytesRead);
+                    }
+
+                    Console.WriteLine("File received and saved as received_file.dat.");
+                }
+
+                client.Close();
+                Console.WriteLine("Client disconnected.");
+            }
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("An error occurred while listening for connections.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
